7ce00dc7ed63812b8f34e8da0dc267d0c470ea2e
Vladislav <rabbiabram@gmail.com>
2014-05-22 23:04:43 +0700

    Update mercator-grid.js
    Added exports

--- mercator-grid.js
+++ mercator-grid.js
@@ -1,5 +1,8 @@
-var MerctorGrid = function(options) {
+var MercatorGrid = function(options) {
     var self = this;
     self.cellSize = options.cellSize || 1;
-    
+}
+
+if (typeof exports == 'function') {
+    exports.MercatorGrid = MercatorGrid;
 }
